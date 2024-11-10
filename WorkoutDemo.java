82564a5272643e3c7341144862629e0919d35c40
Richard T <74796181+rtillies@users.noreply.github.com>
2024-11-10 15:50:53 -0500

    Create two customers

--- WorkoutDemo.java
+++ WorkoutDemo.java
@@ -1,29 +1,21 @@
 // package Workout;
 public class WorkoutDemo {
     public static void main(String args[]) {
-        Customer customer = new Customer("Professor T", 70, 220);
+        Customer rich = new Customer("Professor T", 70.2, 220);
+        Customer bella = new Customer("Lyndsey Bella", 63.5, 140);
 
-        customer.addWorkout();
+        rich.addWorkout();
+        rich.addWorkout(8);
+        rich.addWorkout();
+        rich.addWorkout(6);
+        bella.addWorkout();
+        bella.addWorkout(10);
+        bella.addWorkout();
+        bella.addWorkout(4);
         // customer.finishWorkout();
-        customer.addWorkout(8);
-        // customer.finishWorkout();
-        customer.addWorkout();
-        // customer.finishWorkout();
-        customer.addWorkout(6);
-        // customer.finishWorkout();
-        customer.addWorkout();
-        // customer.finishWorkout();
-        customer.addWorkout(10);
-        // customer.finishWorkout();
-        customer.addWorkout();
-        // customer.finishWorkout();
-        customer.addWorkout(4);
-        // customer.finishWorkout();
-        // customer.addWorkout();
-        // customer.finishWorkout();
-        // Workout workout = new Workout();
 
 
-        System.out.println(customer);
+        System.out.println(rich);
+        System.out.println(bella);
     }
 }
